f9c8647ad4261944c14377ee71130975f7399be5
Chovin Carlson <Chovin@users.noreply.github.com>
2018-08-30 05:57:26 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board()
   board = Array.new 9, " "
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n"\
